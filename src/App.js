852a0552558cad1f11a7206d9644de4637eaf6e9
Deepak  kumar <72205945+dk9284219@users.noreply.github.com>
2023-03-31 07:12:51 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -2,27 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+   <div>
+<h2>Right Tooltip</h2>
+<p>Move the mouse over the text below:</p>
+
+<div className='tooltip'>Hover over me!
+  <span  className='tooltiptext'>Thanks for hovering! I'm a tooltip</span>
+</div>
+</div>
+
   );
 }
 
